b73a21fc3142f9da222a7e212f38a86c79f2c58d
Thennarasu <thennuyukesh@gmail.com>
2017-06-07 12:26:00 +0530

    Add files via upload

---
+++ thennu7.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class positivenegativeorzero
+{ public static void main(String args[])
+{ int num;
+Scanner sc=new Scanner(System.in);
+System.out.print("Enter any integer number: ");
+num=sc.nextInt();
+if(num<0)
+System.out.println(num+"is negative number.");
+else
+System.out.println(num+"is zero.");}}
