2f58c4f8d43319ea19d1f48e8691f0bd8ae9e808
Thennarasu <thennuyukesh@gmail.com>
2017-06-08 15:02:13 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,15 @@
+package ttt;
+import java.util.Scanner;
+public class reverse {
+	
+	public static void main(String[] args) {
+		
+			      String string = "abcdef";
+			      String reverse = new StringBuffer(string).reverse().toString();
+			      System.out.println("\nString before reverse: "+string);
+			      System.out.println("String after reverse: "+reverse);
+			  
+	
+	}
+
+}
