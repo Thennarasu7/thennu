e9a88aaa2244d41ddcf642612a5e10635cc9202c
Thennarasu <thennuyukesh@gmail.com>
2017-07-24 15:22:25 +0530

    Add files via upload

---
+++ ppp.java
@@ -0,0 +1,18 @@
+package ttt;
+
+import java.util.Scanner;
+
+public class ppp {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int a=1;
+		int b=sc.nextInt();
+		for(int i=1;i<=b;i++)
+		{
+			a=a*i;
+		}
+System.out.println("The factorial is:"+a);
+	}
+
+}
