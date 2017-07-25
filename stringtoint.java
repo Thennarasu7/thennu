c531e42b65c963143a7a24388397805b05c7e740
Thennarasu <thennuyukesh@gmail.com>
2017-07-25 10:26:03 +0530

    Add files via upload

---
+++ stringtoint.java
@@ -0,0 +1,14 @@
+package ttt;
+
+public class stringtoint
+{
+
+	public static void main(String[] args)
+	{
+	String str="102";
+	int i=Integer.parseInt(str);
+    System.out.println(str+100);//here integer is not converted to string
+    System.out.println(i+100);//here integer is converted to string
+	}
+
+}
