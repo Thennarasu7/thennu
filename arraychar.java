974b04a2914be945acf90fb14302c1ebf14ac667
Thennarasu <thennuyukesh@gmail.com>
2017-07-25 17:30:59 +0530

    Add files via upload

---
+++ arraychar.java
@@ -0,0 +1,28 @@
+package ttt;
+
+import java.util.Scanner;
+
+public class arraychar {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String p="i am studing in velammal college";
+		int l,j=0;
+		l=sc.nextInt();
+		char c[]=p.toCharArray();
+		int i=0;
+		while(i<c.length)
+		{
+		    j=0;
+		while(j<=l)
+		{
+		System.out.print(c[i++]);
+		j++;
+		}
+		System.out.println("");
+		}
+	
+
+	}
+
+}
