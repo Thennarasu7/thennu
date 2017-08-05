fa23d43a3dde9a13bff9f56e9391bdf9722f8b68
Thennarasu <thennuyukesh@gmail.com>
2017-08-05 15:38:14 +0530

    Add files via upload

---
+++ Online.java
@@ -0,0 +1,15 @@
+package ttt;
+
+import java.util.Scanner;
+
+public class Online
+{
+
+	public static void main(String[] args)
+	{
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	System.out.println(a*a);
+	}
+
+}
